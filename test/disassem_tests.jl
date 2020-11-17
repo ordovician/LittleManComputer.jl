3bea33c23545634ae66416913afcf550a4449703
Erik Engheim <erik.engheim@mac.com>
2020-11-17 03:44:02 +0100

    Minor cleanup for disassembler tests

--- test/disassem_tests.jl
+++ test/disassem_tests.jl
@@ -1,11 +1,12 @@
-datadir = joinpath(dirname(@__FILE__), "data")
-examples = joinpath(dirname(@__FILE__), "../examples")
-
-@testset "Disassembler tests" begin    
-    @testset "Individual instructions" begin
+@testset "Disassembler tests" begin
+   
+    @testset "Without operands" begin
         @test disassemble(901) == "INP"
         @test disassemble(902) == "OUT"
-        @test disassemble(0) == "HLT"
+        @test disassemble(000) == "HLT"                                     
+    end
+    
+    @testset "With operands" begin
         @test disassemble(105) == "ADD 5"
         @test disassemble(112) == "ADD 12"
         @test disassemble(243) == "SUB 43"
@@ -14,5 +15,5 @@ examples = joinpath(dirname(@__FILE__), "../examples")
         @test disassemble(600) == "BRA 0"
         @test disassemble(645) == "BRA 45"
         @test disassemble(782) == "BRZ 82"                                        
-    end    
+    end   
 end
