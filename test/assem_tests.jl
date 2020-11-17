25be237a8863b4c78f49076d56333b7366c43efe
Erik Engheim <erik.engheim@mac.com>
2020-11-17 17:36:15 +0100

    Make nicer for use in code examples
    This is so the lines don't get too long and it is easier to show code in code example, in e.g. a book with limited page width.

--- test/assem_tests.jl
+++ test/assem_tests.jl
@@ -12,10 +12,12 @@ examples = joinpath(dirname(@__FILE__), "../examples")
     end
     
     @testset "Instruction set" begin
-        target = [100, 200, 300, 500, 600, 700, 800, 901, 902, 000]
-        mem = assemble(joinpath(datadir, "instructionset.lmc"))
-        for (i, t) in enumerate(target)
-            @test mem[i] == t
+        instructions = [100, 200, 300, 500, 600, 
+                        700, 800, 901, 902, 000]
+        filepath = joinpath(datadir, "instructionset.lmc")
+        program = assemble(filepath)
+        for (i, expected) in enumerate(instructions)
+            @test program[i] == expected
         end
     end
     
